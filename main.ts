ed737fc29f7a22f3d1d5bd52d48d926d25485b63
Davide Luppoli <davide@davideluppoli.it>
2024-06-03 12:50:55 +0200

    MainMenuView

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
+import { MainMenuView } from "./views/MainMenuView";
+
 //Entry point applicazione
+MainMenuView.show();
---
+++ views/MainMenuView.ts
@@ -0,0 +1,38 @@
+import * as rl from 'readline-sync'
+import { ParcheggiController } from '../controllers/ParcheggiController';
+
+export class MainMenuView
+{
+    static show() : void
+    {
+        let controller = new ParcheggiController();
+
+        let scelta : number = 0;
+        do
+        {
+            console.log('GESTIONE PARCHEGGIO')
+            console.log('-------------------')
+            console.log('')
+            console.log('1 - Elenco veicoli')
+            console.log('2 - Numero veicoli presenti')
+            console.log('')
+            console.log('9 - Esci')
+            console.log('')
+
+            scelta = rl.questionInt('Inserire la scelta:')
+
+            switch(scelta)
+            {
+                case 1:
+                    controller.getAll().forEach(v => console.log(`${v.targa} - Entrato: ${v.ingresso}`) )
+                    break;
+                case 2:
+                    break;
+                case 9:
+                    break;
+                default:
+                    console.log('Inserire un numero valido');
+            }
+        } while (scelta!=9)
+    }
+}
