9503ebd4d8bd99858b9ba0a351a2412a8ca4623c
Davide Luppoli <davide@davideluppoli.it>
2024-06-03 12:04:08 +0200

    Parcheggi Controller

---
+++ controllers/ParcheggiController.ts
@@ -0,0 +1,21 @@
+import { Database } from "../models/Database";
+import { Veicolo } from "../models/Veicolo";
+
+export class ParcheggiController
+{
+    private db : Database;
+    constructor()
+    {
+        this.db = new Database();
+    }
+
+    getAll() : Veicolo[]
+    {
+        return this.db.parcheggio.veicoli.filter(v => v.uscita==undefined);
+    }
+
+    getNumber() : number
+    {
+        return this.getAll().length;
+    }
+}
